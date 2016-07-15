9422d223ac115b18f445660222b7a2701d6b297a
Alastair Montgomery <amontgomery@williamhill.co.uk>
2016-07-15 08:39:28 +0100

    added logic to join second manager to the master

--- modules/swarm-manager/manifests/init.pp
+++ modules/swarm-manager/manifests/init.pp
@@ -3,8 +3,16 @@ class swarm-manager {
 
         $discovery = hiera('discovery')
         $secret = hiera('secret')
+        $manager = hiera('manager')
 
-	exec {'manager start':
-	   command => "/usr/bin/docker swarm init --listen-addr $ipaddress_eth1:2377 --secret $secret",
-	}
+	if $ipaddress_eth1 == $manager {
+	    exec {'manager start':
+	       command => "/usr/bin/docker swarm init --listen-addr $ipaddress_eth1:2377 --secret $secret --auto-accept worker,manager",
+	    }
+        }
+        else {
+            exec { 'swarm join':
+                command => "/usr/bin/docker swarm join --secret $secret --manager --listen-addr $ipaddress_eth1:2377 $manager:2377",
+            }
+        }
 }
